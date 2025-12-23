7a35f22a42929baa293a9f6b229e1ddb269b3cc2
Ngô Minh Trung <trung.nm226004@sis.hust.edu.vn>
2025-12-23 14:43:50 +0700

    add data visual

--- pages/1_Data_Visualization.py
+++ pages/1_Data_Visualization.py
@@ -1,3 +1,74 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
-st.write('Data')
+df = pd.read_csv('df_clean.csv')
+
+def house_by_city():
+    city_counts = df['city'].value_counts()
+    top_3_cities = city_counts.head(3)
+    other_cities_count = city_counts.iloc[3:].sum()
+    if other_cities_count > 0:
+        plot_data = pd.concat([top_3_cities, pd.Series({'Others': other_cities_count})])
+    else:
+        plot_data = top_3_cities
+        
+    plt.figure(figsize=(10, 8))
+    plt.pie(plot_data, labels=plot_data.index, autopct='%1.1f%%', startangle=180, colors=['navy', 'steelblue', 'skyblue', 'lightgray']
+, textprops={'color': 'white'})
+    plt.title('Distribution of Houses by City (Top 3 + Others)')
+    plt.axis('equal')  
+    return plt
+
+
+def house_by_type():
+    type_counts = df['house_type'].value_counts()
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=type_counts.index, y=type_counts.values, color = 'lightcoral')
+    plt.title('Distribution of Houses by Type')
+    plt.xlabel('House Type')
+    plt.ylabel('Number of Houses')
+    plt.xticks(rotation=45)
+    return plt
+
+def price_distribution():
+    plt.figure(figsize=(10, 6))
+    sns.histplot(df['price'], kde=True, bins=30, color = 'steelblue')
+    plt.title('Distribution of Price')
+    plt.xlabel('Price (billion VND)')
+    plt.ylabel('Number of Houses')
+    return plt
+
+def size_distribution():
+    plt.figure(figsize=(10, 6))
+    sns.histplot(df['size'], kde=True, bins=30, color = 'crimson')
+    plt.title('Distribution of House Sizes')
+    plt.xlabel('Size (m^2)')
+    plt.ylabel('Number of Houses')
+    return plt
+
+
+def main():
+    st.set_page_config(layout="wide")
+    st.title("House Data Visualization")
+
+    st.subheader("Distribution of House Sizes")
+    size_dist = size_distribution()
+    st.pyplot(size_dist)
+    
+    st.subheader("Distribution of Price")
+    price_dist = price_distribution()
+    st.pyplot(price_dist)
+
+    st.subheader("Distribution of Houses by City")
+    city_dist = house_by_city()
+    st.pyplot(city_dist)
+
+    st.subheader("Distribution of Houses by Type")
+    type_dist = house_by_type()
+    st.pyplot(type_dist)
+
+if __name__ == "__main__":
+    main()
